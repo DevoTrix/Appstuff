a9425f8dc0651c8be18410a421d57762a23de780
Joseph Chang <jchan443@ucr.edu>
2024-11-07 20:35:42 -0800

    changed main app to be stateful.

--- dating_app/lib/main.dart
+++ dating_app/lib/main.dart
@@ -1,20 +1,27 @@
+import 'package:dating_app/pages/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  _MainAppState createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      title:'Dating App Title TBD',
+      home: LoginPage(), // this is temporary i think should have a home page thats not the login page.
     );
+  }
+  
+  Future<void> loginAction() async {
+
   }
 }
---
+++ dating_app/lib/pages/login.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Log In',
+           style: TextStyle(
+            
+           )
+        )
+      )
+    );
+  }
+
+  
+}
